8a3120b404fb5999d2b25179a174ba8eba82b1b1
yasat <ghantasat007@gmail.com>
2020-11-18 19:49:32 +0530

    Update LinkedMid.java
    adding assignment 2 files

--- assignment_2/satwik/6/assignment2/LinkedMid.java
+++ assignment_2/satwik/6/assignment2/LinkedMid.java
@@ -7,6 +7,6 @@ public class LinkedMid {
 	public static void main(String[] args) {
 		List<Integer> lkl = new LinkedList<>();
 		lkl.add(10);lkl.add(20);lkl.add(30);lkl.add(40);lkl.add(50);lkl.add(60);
-		System.out.println(lkl.get((lkl.size()/2))); //Since indexing start from 0, we don't have to add 1 to get middle or second mid.
+		System.out.println(lkl.get((lkl.size()/2))); //Since indexing start from 0, we don't have to add 1 to get middle or second mid
 	}
 }
