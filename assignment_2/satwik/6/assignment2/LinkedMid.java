10110843946d9bddf16d56473ff459ecf9c186d6
yasat <ghantasat007@gmail.com>
2020-11-18 20:03:30 +0530

    adding assignment 2 files

--- assignment_2/satwik/6/assignment2/LinkedMid.java
+++ assignment_2/satwik/6/assignment2/LinkedMid.java
@@ -7,6 +7,16 @@ public class LinkedMid {
 	public static void main(String[] args) {
 		List<Integer> lkl = new LinkedList<>();
 		lkl.add(10);lkl.add(20);lkl.add(30);lkl.add(40);lkl.add(50);lkl.add(60);
-		System.out.println(lkl.get((lkl.size()/2))); //Since indexing start from 0, we don't have to add 1 to get middle or second mid. if first mid is needed, we can check for even or odd with simple if and subtract 1 from even length
+
+		System.out.println(lkl.get((lkl.size()/2)));
+		//Since indexing start from 0, we don't have to add 1 to get middle or second mid.
+		
+		//for first mid. we can check for even and subtract 1 from index
+		if(lkl.size()%2==0) {
+			System.out.println(lkl.get((lkl.size()/2)-1));
+		}
+		else {
+			System.out.println(lkl.get((lkl.size()/2)));
+		}
 	}
 }
